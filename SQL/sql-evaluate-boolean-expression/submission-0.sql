a22b38eab8682e42f5ed2b01d8ff730b8affa9f8
salma tammari <salmatammari2004@gmail.com>
2026-09-20 13:41:41 +0100

    Add: sql-evaluate-boolean-expression - submission-0

---
+++ SQL/sql-evaluate-boolean-expression/submission-0.sql
@@ -0,0 +1,11 @@
+select e.left_operand , e.operator, e.right_operand  , 
+case 
+when operator = '=' and v1.value = v2.value then true
+when operator = '>' and v1.value > v2.value then true
+when operator = '<' and v1.value < v2.value then true
+else false 
+end  as value 
+from  expressions  e 
+              join variables v1 on v1.name =e.left_operand 
+              join variables v2 on v2.name=e.right_operand
+
